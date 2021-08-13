f1e0f7334a2b9db0b3f1c3b40bce8ab925dc52ae
Pedro <pedro.diniz@zup.com.br>
2021-08-13 12:25:09 -0300

    Primeiro commit, projeto criado.

---
+++ src/test/java/br/com/zup/desafioproposta/DesafioPropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.desafioproposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
